8ea3adf5d807e45e12237722d12b0815f8aa4b19
Niranjan Uma Shankar <niranjan.uma.shankar@automattic.com>
2019-03-11 10:58:01 +0530

    403 response code for OPML

--- modules/sitemaps.php
+++ modules/sitemaps.php
@@ -19,6 +19,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+error_log('in sitemap, blog public is ' . get_option( 'blog_public' ));
 if ( '1' == get_option( 'blog_public' ) ) { // loose comparison okay.
 	include_once 'sitemaps/sitemaps.php';
 }
